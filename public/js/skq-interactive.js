bdc23523fa1a3db5c7dd931062be68a351c1735c
Seth Terpstra <seth@sethterpstra.com>
2015-07-24 04:47:02 -0400

    wip navbar fixes css organization

--- public/js/skq-interactive.js
+++ public/js/skq-interactive.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
 	var body = $("html, body");
-	$(body).scrollTop(0);
+	// $(body).scrollTop(0);
+	collapseHeader();
 
 	function collapseHeader() {
     var pageScroll = $(this).scrollTop();
@@ -12,6 +13,7 @@ $(document).ready(function() {
         $(".navbar").css({'min-height' : 50});
 
         $(".skq, .skq-nav-link").css({'padding-top' : 49-(pageScroll*0.28333333)});
+        $(".navbar-toggle").css({'margin-top' : 43-(pageScroll*0.28333333)});
 
         $(".skq").css({'font-size' : 47-(pageScroll*0.18333333)});
         $(".skq-tagline").css({'opacity' : 1-(pageScroll*0.058333333)});
@@ -19,11 +21,19 @@ $(document).ready(function() {
     if(pageScroll > 120) {
         $(".navbar, .navbar-header, .skq-header").css({'height' : 50});
         $(".skq, .skq-nav-link").css({'padding-top' : 15});
+        $(".navbar-toggle").css({'margin-top' : 9});
+
         $(".skq").css({'font-size' : 25});
+        $(".skq-tagline").css({'opacity' : 0});
         $(".navbar").addClass('nav-mini');
+        $(".navbar").css({'min-height' : 50});
+        
     }
     if(pageScroll < 5) {
-        $(".skq").css({'padding-top' : 49});
+        $(".navbar, .navbar-header, .skq-header").css({'height' : 120});
+        $(".skq, .skq-nav-link").css({'padding-top' : 49});
+        $(".navbar-toggle").css({'margin-top' : 43});
+
         $(".skq").css({'font-size' : 47});
         $(".skq-tagline").css({'opacity' : 1});
         $(".navbar").removeClass('nav-mini');
