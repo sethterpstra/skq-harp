95ef0da98dd651736e98bfc5da9bd11be292ea75
Seth Terpstra <seth@sethterpstra.com>
2015-07-25 16:03:46 -0400

    added pro and ent pages, changed file structure

--- public/js/skq-interactive.js
+++ public/js/skq-interactive.js
@@ -7,9 +7,11 @@ $(document).ready(function() {
 	function collapseHeader() {
     var pageScroll = $(this).scrollTop();
     var elementPadding = $('a.skq').css('padding-top').replace(/[^-\d\.]/g, '');
+    var headerHeight = 120
 
-    if(pageScroll > 0 && pageScroll < 120 ) {
-        $(".navbar, .navbar-header, .skq-header").css({'height' : 120-(pageScroll*0.58333333)});
+    // Animate collapse attached to scrolling position
+    if(pageScroll > 0 && pageScroll < headerHeight ) {
+        $(".navbar, .navbar-header, .skq-header").css({'height' : headerHeight-(pageScroll*0.58333333)});
         $(".navbar").css({'min-height' : 50});
 
         $(".skq, .skq-nav-link").css({'padding-top' : 49-(pageScroll*0.28333333)});
@@ -18,27 +20,31 @@ $(document).ready(function() {
         $(".skq").css({'font-size' : 47-(pageScroll*0.18333333)});
         $(".skq-tagline").css({'opacity' : 1-(pageScroll*0.058333333)});
     }
-    if(pageScroll > 120) {
+    // Set to collapsed after scrolling past headerHeight
+    if(pageScroll > headerHeight) {
         $(".navbar, .navbar-header, .skq-header").css({'height' : 50});
+        $(".navbar").addClass('nav-mini');
+        $(".navbar").css({'min-height' : 50});
+        
         $(".skq, .skq-nav-link").css({'padding-top' : 15});
         $(".navbar-toggle").css({'margin-top' : 9});
 
         $(".skq").css({'font-size' : 25});
         $(".skq-tagline").css({'opacity' : 0});
-        $(".navbar").addClass('nav-mini');
-        $(".navbar").css({'min-height' : 50});
         
     }
+    // Reset elements at scrolltop
     if(pageScroll < 5) {
-        $(".navbar, .navbar-header, .skq-header").css({'height' : 120});
+        $(".navbar, .navbar-header, .skq-header").css({'height' : headerHeight});
+        $(".navbar").removeClass('nav-mini');
+        
         $(".skq, .skq-nav-link").css({'padding-top' : 49});
         $(".navbar-toggle").css({'margin-top' : 43});
 
         $(".skq").css({'font-size' : 47});
         $(".skq-tagline").css({'opacity' : 1});
-        $(".navbar").removeClass('nav-mini');
     }
-	};
+};
 
 
   $('#client-carousel').scrollingCarousel({
